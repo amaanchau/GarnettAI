d7bc512c34158fa607b14f359f9673b45b34836c
Amaan Chaudhry <amaanchau7@gmail.com>
2025-04-10 22:59:06 -0500

    Update Navbar.tsx

--- garnett_frontend/src/components/Navbar.tsx
+++ garnett_frontend/src/components/Navbar.tsx
@@ -73,7 +73,7 @@ export default function Navbar({ onNewChat, conversationStarted }: NavbarProps) 
             {isMenuOpen && (
                 <div className="md:hidden pt-8 pb-6 border-t border-gray-100 font-[family-name:var(--font-geist-sans)]">
                     <div className="flex flex-col space-y-4">
-                        <Link href="/aggie-ai" className="py-4 px-5 text-xl text-gray-600 rounded transition-colors duration-300 ease-in-out hover:bg-gray-100 hover:text-gray-300 flex items-center">
+                        <Link href="/" className="py-4 px-5 text-xl text-gray-600 rounded transition-colors duration-300 ease-in-out hover:bg-gray-100 hover:text-gray-300 flex items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
                             </svg>
